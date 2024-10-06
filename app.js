595aba061d1ec10889f6b0a8e0bab7253ca98713
Santiadi <disacamo43@gmail.com>
2024-10-06 16:15:50 -0500

    gateway-v2.1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const imageRoutes = require('./routes/routes.js');
 
 const app = express();
-const port = 3015;
+const port = 3510;
 
 app.use('/img', imageRoutes);
 
--- routes/routes.js
+++ routes/routes.js
@@ -3,16 +3,7 @@ const router = express.Router();
 const upload = require('../middlewares/uploads.js');
 const { processImage, getBase64Image } = require('../controllers/controllers.js');
 
-router.post('/comment', upload.single('image'), processImage, (err, req, res, next) => {
-    if (err instanceof multer.MulterError) {
-        // Error de Multer
-        return res.status(500).json({ error: `Multer error: ${err.message}` });
-    }
-    if (err) {
-        return res.status(500).json({ error: `Error general: ${err.message}` });
-    }
-    next();
-});
+router.post('/comment', upload.single('image'), processImage);
 
 router.get('/base64-comment', getBase64Image);
 
